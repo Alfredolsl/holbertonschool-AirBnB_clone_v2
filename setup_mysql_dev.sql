bd580e884276a24eeff7a189816e7756d6d64244
root <alfredo.445566@gmail.com>
2024-03-27 01:17:43 +0000

    script that prepares a MySQL server for the project

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,10 @@
 -- Prepares a MySQL server for the HBNB.
 -- A db hbtn_dev_db
-CREATE DATABASE IF NOT EXISTS hbtn_dev_db;
+CREATE DATABASE IF NOT EXISTS 'hbtn_dev_db';
 
 -- A new user hbnb_dev (in localhost)
 -- Password for hbnbn dev called hbnb_dev_pwd
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- hbnb_dev all privileges ONLY for db hbnb_dev_db
 GRANT ALL PRIVILEGES ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
