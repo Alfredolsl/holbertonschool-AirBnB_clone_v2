a7904ec8231b2d33ca2339000ddde10323ca3b6e
root <alfredo.445566@gmail.com>
2024-03-28 18:26:42 +0000

    added comments inside reload methods

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,7 +2,15 @@
 """Database Storage for HBNB project."""
 from os import getenv
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+
 
 class DBStorage:
     """Create tables"""
@@ -27,9 +35,38 @@ class DBStorage:
         """Returns a dictionary"""
         queryDict = {}
         if cls:
-            query = self.__session.query(cls)
+            query = DBStorage.__session.query(cls)
             for elem in query:
                 key = "{}.{}".format(type(elem).__name__, elem.id)
                 queryDict[key] = elem
         else:
-            
+           allClasses = [State, City, User, Place, Review, Amenity]
+           for cls in allClasses:
+               query = DBStorage.__session.query(cls)
+               for elem in query:
+                   key = "{}.{}".format(type(elem).__name__, elem.id)
+                   queryDict[key] = elem
+        return queryDict
+
+    def new(self, obj):
+        """adds a new element to the table"""
+        DBStorage.__session.add(obj)
+
+    def save(self):
+        """saves changes to the table"""
+        DBStorage.__session.commit()
+
+    def delete(self, obj=None):
+        if obj:
+            DBStorage.__session.delete(obj)
+
+    def reload(self):
+        """Creates the current session from the db engine."""
+        # creates table
+        Base.metadata.create_all(self.__engine)
+        # establishes calls to the db
+        currentSession = sessionmaker(bind=self.__engine, expire_on_commit=False) 
+        # scoped_session produces a managed registry of Session objects (thread safety)
+        # threads manage user requests and db operations.
+        Session = scoped_session(currentSession)
+        DBStorage.__session = Session()
