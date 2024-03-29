04cb50a475fa5a84fb0514f724dab3ef24ca0f8c
root <alfredo.445566@gmail.com>
2024-03-29 23:12:19 +0000

    fixed typos

--- models/state.py
+++ models/state.py
@@ -4,7 +4,6 @@ from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-import models
 from models.city import City
 
 
@@ -12,7 +11,8 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete-orphan", backref="state")
+    cities = relationship("City", cascade="all, delete-orphan",
+                          backref="state")
 
     @property
     def cities(self):
@@ -23,7 +23,6 @@ class State(BaseModel, Base):
         #for city in extracted_cities:
         #    if self.id == city.state_id:
         #        my_list.append(city)
-        #print("list of cities:", my_list)
         #return my_list
 
         all_objects = models.storage.all()
@@ -32,5 +31,4 @@ class State(BaseModel, Base):
         for key, val in all_objects.items():
             if "City" in key:
                 filtered_cities.append(key[val])
-        print("got cities")
         return [city for city in filtered_cities if city.state_id == self.id]
