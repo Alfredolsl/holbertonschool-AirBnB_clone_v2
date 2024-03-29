17fc27150aa20f469b1a33d8c01741b886e48807
root <alfredo.445566@gmail.com>
2024-03-29 21:14:46 +0000

    new_instance is now saved into the storage instead of the storage itself

--- console.py
+++ console.py
@@ -135,7 +135,7 @@ class HBNBCommand(cmd.Cmd):
                     setattr(new_instance, key, eval(value))
 
         print(new_instance.id)
-        storage.save()
+        new_instance.save()
 
     def help_create(self):
         """ Help information for the create method """
