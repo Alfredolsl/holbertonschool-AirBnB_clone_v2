15346866f6e29d8e79595a3e5b50307a57a0b3db
root <alfredo.445566@gmail.com>
2024-04-11 01:12:58 +0000

    fixed typo

--- models/place.py
+++ models/place.py
@@ -5,11 +5,16 @@ from sqlalchemy import Table, Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 
 place_amenity = Table("place_amenity", Base.metadata,
-                      Column("place_id", String(60), ForeignKey("places.id"),
-                             primary_key=True, nullable=False),
-                      Column("amenity_id", String(60),
+                      Column("place_id",
+                             String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True,
+                             nullable=False),
+                      Column("amenity_id",
+                             String(60),
                              ForeignKey("amenities.id"),
-                             primary_key=True, nullable=False))
+                             primary_key=True,
+                             nullable=False))
 
 
 class Place(BaseModel, Base):
